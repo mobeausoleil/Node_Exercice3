753457c95703a1bb3464e47ce6c718815f58c6fc
Maurice Beausoleil <kool_momo_kool@hotmail.com>
2018-02-02 15:51:04 -0500

    Exercice terminé

--- serveur.js
+++ serveur.js
@@ -1,17 +1,26 @@
 const http = require('http');
 const port = 3000;
 const fs = require('fs');
+let provinces;
+
+fs.readFile('province.json', (err, data) => {
+		if (err) return console.error(err);
+		provinces = JSON.parse(data);
+		return provinces;
+});
 
 http.createServer((request, response) => {
 
-	let provinces = fs.readFile('province.json', 'utf8', (err, data) => {
-	  
-		if (err) return console.error(err);
-		return JSON.parse(data);
-	});
-
 	console.log(provinces);
+	let html = "<!DOCTYPE html><head><title>Les provinces</title><meta charset='utf-8'></head><body><table>";
 
+	for (let nom in provinces){
+		html+= "<tr><td>"+provinces[nom]+"</td></tr>";
+	}
+
+	html+="</table></body></html>"
+
+	response.write(html);
 	response.end();
 
 }).listen(port)
